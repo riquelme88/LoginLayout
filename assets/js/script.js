cc9ec05d582c90d8b19472aeebd268ab342ba8ac
Riquelme <riquelmestayler@gmail.com>
2023-07-03 23:07:00 -0300

    Html e css finalizado, somente arrumar os botões dps

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+let inputs = document.querySelectorAll('input')
+
+ function clickInput0(e){
+    switch(e.target){
+        case e.target:
+            inputs[0].style.border = '1px solid #d522fa'
+            break;
+            case e.target = window:
+                inputs[0].style.border = '10px solid #df1'
+    }
+ }
+
+ function clickInput1(e){
+    switch(e.target){
+        case e.target:
+            inputs[1].style.border = '1px solid #d522fa'
+            break;
+            case e.target = window:
+                inputs[1].style.border = '10px solid #df1'
+    }
+ }
